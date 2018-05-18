ed99d14554e93c38cb5e3f85df06bb24c60d67c5
bilalbentoumi <bilalbentoumi@gmail.com>
2018-05-18 14:10:21 +0100

    Update readmore.js

--- readmore.js
+++ readmore.js
@@ -4,21 +4,24 @@ posts_thumb_sum = 400;
 img_thumb_height = 160;
 img_thumb_width = 180;
 
-function removeHtmlTag(strx,chop){
-if(strx.indexOf("<")!=-1)
-{
-var s = strx.split("<");
-for(var i=0;i<s.length;i++){
-if(s[i].indexOf(">")!=-1){
-s[i] = s[i].substring(s[i].indexOf(">")+1,s[i].length);
-}
-}
-strx = s.join("");
-}
-chop = (chop < strx.length-1) ? chop : strx.length-2;
-while(strx.charAt(chop-1)!=' ' && strx.indexOf(' ',chop)!=-1) chop++;
-strx = strx.substring(0,chop-1);
-return strx+'...';
+function getSummary(strx, chop) {
+    if(strx.indexOf("<") != -1) {
+        var s = strx.split("<");
+        for(var i = 0; i < s.length; i++) {
+            if(s[i].indexOf(">") != -1) {
+                s[i] = s[i].substring(s[i].indexOf(">")+1, s[i].length);
+            }
+        }
+        strx = s.join("");
+    }
+
+    chop = (chop < strx.length-1) ? chop : strx.length-2;
+
+    while(strx.charAt(chop-1) != ' ' && strx.indexOf(' ', chop) != -1)
+        chop++;
+
+    strx = strx.substring(0, chop-1);
+    return strx + '...';
 }
 
 function createSummaryAndThumb(post_id, post_url, post_title) {
@@ -30,9 +33,6 @@ function createSummaryAndThumb(post_id, post_url, post_title) {
         imgtag = `<span class="posts-thumb" style="float:left; margin-right: 10px;"><a href="${post_url}" title="${post_title}"><img src="${img[0].src}" width="${img_thumb_width}px" height="${img_thumb_height}px" /></a></span>`;
         summ = posts_thumb_sum;
     }
-
-    var summary = imgtag + '<div>بسم الله الرحمان الرحيم</div>';
-    post_content.innerHTML = summary;
+    post_content.innerHTML = imgtag + '<div>' + getSummary(post_content.innerHTML, summ) + '</div>';
 }
-
 //]]>
