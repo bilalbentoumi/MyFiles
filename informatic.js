228a5ed3f4c6fb8da570c2a6e3ca8b87e0aed17d
bilalbentoumi <bilalbentoumi@gmail.com>
2018-05-18 10:49:18 +0100

    Update informatic.js

--- informatic.js
+++ informatic.js
@@ -1,58 +1,36 @@
+/* Toggle Navbar */
 function toggleNav() {
+    if ($('.toggle').hasClass('show')) {
+        toggle('.wrapper', 'hide');
+        $(window).scrollTop(parseInt($('#scroll_position').val()));
+    } else {
+        $('#scroll_position').val($(window).scrollTop());
+        setTimeout(function(){
+            toggle('.wrapper', 'hide');
+        }, 400);
+    }
+    toggle('.toggle', 'show');
+}
 
-            if ($('.toggle').hasClass('show')) {
-                toggle('.wrapper', 'hide');
-                $(window).scrollTop(parseInt($('#scroll_position').val()));
-            } else {
-                $('#scroll_position').val($(window).scrollTop());
-                setTimeout(function(){
-                    toggle('.wrapper', 'hide');
-                }, 1000);
-            }
+/* Toggle Class */
+function toggle(e, c) {
+    $(e).toggleClass(c);
+}
 
-            toggle('.toggle', 'show');
+/* Scroll Navbar */
+$(window).scroll(function(){
+    var sticky = $('.navbar'),
+      scroll = $(window).scrollTop();
 
-        }
+    if (scroll >= 100)
+        sticky.addClass('navbar-dark');
+    else
+        sticky.removeClass('navbar-dark');
+});
 
-        function toggle(e, c) {
-            $(e).toggleClass(c);
-        }
-
-        $(window).scroll(function(){
-          var sticky = $('.navbar'),
-              scroll = $(window).scrollTop();
-
-          if (scroll >= 100)
-    		sticky.addClass('navbar-dark');
-          else
-    		sticky.removeClass('navbar-dark');
-        });
-
-        if (screen.width <= 991) {
-            $('.toggle').height(screen.height - $('.navbar-header').outerHeight());
-        }
-
-        window.onresize = function(event) {
-    		if (screen.width <= 991) {
-            	$('.toggle').height(screen.height - $('.navbar-header').outerHeight());
-                if ($('.toggle').hasClass('show'))
-                    $('.wrapper').addClass('hide');
-            } else {
-                $('.toggle').height('auto');
-                $('.wrapper').removeClass('hide');
-            }
-        };
-
-        window.onscroll = function(event) {
-    		if (screen.width <= 991) {
-            	$('.toggle').height(screen.height - $('.navbar-header').outerHeight());
-            } else {
-                $('.toggle').height('auto');
-            }
-        };
-
-        $(".social").hover(function() {
-            $(this).addClass('social-focus').siblings().addClass('social-sib');
-        }, function() {
-            $(this).removeClass('social-focus').siblings().removeClass('social-sib');
-        });
+/* Social Icons Hover */
+$(".social").hover(function() {
+    $(this).addClass('social-focus').siblings().addClass('social-sib');
+}, function() {
+    $(this).removeClass('social-focus').siblings().removeClass('social-sib');
+});
