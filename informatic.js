69906480f0027aa8d0a28869732610b25ab15f3b
bilalbentoumi <bilalbentoumi@gmail.com>
2018-05-26 22:59:51 +0100

    Update informatic.js

--- informatic.js
+++ informatic.js
@@ -1,14 +1,7 @@
-//var scroll_position = 0;
-
 /* Toggle Navbar */
 function toggleNav() {
-    toggle('body', 'hide');
-    toggle('.toggle', 'show');
-}
-
-/* Toggle Class */
-function toggle(e, c) {
-    $(e).toggleClass(c);
+    $('body').toggleClass('hide');
+    $('.toggle').toggleClass('show');
 }
 
 /* Scroll Navbar */
@@ -20,11 +13,4 @@ $(window).scroll(function(){
         sticky.addClass('navbar-dark');
     else
         sticky.removeClass('navbar-dark');
-});
-
-/* Social Icons Hover */
-$(".social").hover(function() {
-    $(this).addClass('social-focus').siblings().addClass('social-sib');
-}, function() {
-    $(this).removeClass('social-focus').siblings().removeClass('social-sib');
 });
