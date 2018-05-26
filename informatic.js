f9b818cb2307aebec18fe6c9224544c6c97135a0
bilalbentoumi <bilalbentoumi@gmail.com>
2018-05-26 23:47:08 +0100

    Update informatic.js

--- informatic.js
+++ informatic.js
@@ -205,3 +205,29 @@ function initPostHeader(title, img_url) {
 	$('.post-title').html(title);
 	$('.post-image').attr('src', img_url);
 }
+/* Initialize Post Header */
+$(function(){
+	$.each($('a[name]'), function(i, e) {
+		var elem = $(e).parent().find('#postviews');
+		var blogStats = new Firebase('https://YOUR-APP-NAME.firebaseio.com/pages/id/' + $(e).attr('name'));
+		blogStats.once('value', function(snapshot) { 
+		    var data = snapshot.val();
+		    var isnew = false;
+		    if(data == null) {
+			data= {};
+			data.value = 0;
+			data.url = window.location.href;
+			data.id = $(e).attr('name');
+			isnew = true;
+		    }
+		    elem.text(data.value);
+		    data.value++;
+		    if(window.location.pathname != '/') {
+			if(isnew)
+			    blogStats.set(data);
+			else
+			    blogStats.child('value').set(data.value);
+		    }
+		});
+	});
+});
